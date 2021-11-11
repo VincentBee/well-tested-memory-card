1d7227ef95a1859badef545c869c928c9a7ad211
Vincent <vincentb45@gmail.com>
2021-11-11 09:30:03 +0100

    test: display card when click on play

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('Memory card game', () => {
@@ -7,5 +8,11 @@ describe('Memory card game', () => {
         render(<App />);
         const linkElement = screen.getByText(/play/i);
         expect(linkElement).toBeInTheDocument();
+    });
+
+    it('should display 12 non visible cards when start new game', () => {
+        render(<App />);
+        userEvent.click(screen.getByText('play'))
+        expect(screen.getAllByRole('card')).toHaveLength(12);
     });
 })
